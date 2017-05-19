57acaec480c15f2776160dc94ebe2127f885cba4
Josh Cox <josh@webhosting.coop>
2017-05-19 13:08:59 -0500

    ws

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ wget $KEY_URL -O /tmp/rootsshauthorized_keys
 cat /tmp/rootsshauthorized_keys|sort|uniq>/tmp/rootsshauthorized_keysuniq
 rm /tmp/rootsshauthorized_keys
 mv /tmp/rootsshauthorized_keysuniq /root/.ssh/authorized_keys
+
 chmod 600 /root/.ssh/authorized_keys
 
 /usr/sbin/sshd -D
